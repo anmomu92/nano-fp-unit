a5e4e26e8ed9f9fc8818e6171d80c72c27cffee6
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-06-03 12:27:44 +0200

    added: exp_diff.sv module.

---
+++ src/exp_diff.sv
@@ -0,0 +1,15 @@
+module exp_diff #(
+    WIDTH = 32
+)(
+    input wire clk,
+    input wire rst_n,
+
+    input logic exp_A,
+    input logic exp_B,
+    output logic sel,
+    output logic n
+);
+
+
+
+endmodule
